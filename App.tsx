40c29720e9bdbe779b0b0da8dd95a3efabca0b71
unknown <juanf.herdoia@gmail.com>
2022-05-20 09:47:15 -0500

    inicio appnavigator

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import {NavigationContainer} from '@react-navigation/native';
+import React from 'react';
+
+// import {NavigationContainer} from '@react-navigation/native';
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <View>
+        <Text> App Screen</Text>
+      </View>
+    </NavigationContainer>
+  );
+};
+
+export default App;
